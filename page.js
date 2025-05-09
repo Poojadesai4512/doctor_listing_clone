c8f6b316a49872c0b5760a385afb1e57e9aec3fd
Pooja Desai <158258670+Poojadesai4512@users.noreply.github.com>
2025-05-09 12:17:13 +0530

    Add files via upload

---
+++ page.js
@@ -0,0 +1,9 @@
+export default function HomePage() {
+    return (
+      <main style={{ padding: "2rem", fontFamily: "Arial" }}>
+        <h1>Doctor Listing App 🏥</h1>
+        <p>Welcome! Frontend is connected successfully.</p>
+      </main>
+    );
+  }
+  
